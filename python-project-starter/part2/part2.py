917d33280d6ea17702fb6aeaadbf770e57b1e6cf
Joy Taylor <jcat7414@gmail.com>
2020-08-01 21:04:41 +1000

    First attempt at part 2

--- python-project-starter/part2/part2.py
+++ python-project-starter/part2/part2.py
@@ -2,5 +2,11 @@ import json
 import plotly.express as px
 import pandas as pd
 
+with open("data/forecast_5days_a.json") as json_file:
+    forecast = json.load(json_file)
+
 df = pd.read_json("data/forecast_5days_a.json")
 
+# for item in json_file["DailyForecasts"]:
+fig = px.line(df, x=(item["Temperature"]['Minimum']["Value"]), y=(item["Date"]), title="Minimum Temperature")
+fig.write_html(min_temp.html)
